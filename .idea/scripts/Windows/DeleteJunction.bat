9d72ea7a390cc9c2f95f1ac1f08af59fbeee3214
Zhou Fang <houshuu@live.jp>
2018-11-09 17:18:47 +0900

    Add unix link script

--- .idea/scripts/Windows/DeleteJunction.bat
+++ .idea/scripts/Windows/DeleteJunction.bat
@@ -1,5 +1,4 @@
 @echo off
-cd /d %~dp0..\..\
 if exist "%USERPROFILE%\go\src\github.com\fang2hou\easyga" (
     rd "%USERPROFILE%\go\src\github.com\fang2hou\easyga"
     echo "Deleted."
---
+++ .idea/scripts/macOS_Linux/DeleteLink.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~
+if [ ! -f "go/src/github.com/fang2hou/easyga" ]; then
+	rm ~/go/src/github.com/fang2hou/easyga
+fi
