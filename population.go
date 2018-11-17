aa3fb9a9eacbba3120825c5acc057bfbc1598986
Zhou Fang <houshuu@live.jp>
2018-11-17 17:10:52 +0900

    Update poplation object

--- population.go
+++ population.go
@@ -6,33 +6,38 @@ type population struct {
 	Chromosomes []Chromosome
 }
 
+// getRandomChromosomeIndex is a private method that return a random chromosome index
 func getRandomChromosomeIndex(p *population) int {
 	return rand.Int() % len(p.Chromosomes)
 }
 
+// Init is a method to generate the first iteration
 func (p *population) Init(length int, size int, genotype uint8, initFunc func(c *Chromosome)) {
+	// Initialize population
 	p.Chromosomes = make([]Chromosome, 0)
-	if initFunc != nil {
-		for i := 0; i < size; i++ {
-			var tempChromosome Chromosome
-			initFunc(&tempChromosome)
-			p.Chromosomes = append(p.Chromosomes, tempChromosome)
-		}
 
-		return
-	}
-
+	// Create new individuals
 	for i := 0; i < size; i++ {
+		// Initialize a new chromosome
 		var tempChromosome Chromosome
-		tempChromosome.Random(length, genotype)
+		// Generate gene
+		if initFunc != nil {
+			initFunc(&tempChromosome)
+		} else {
+			tempChromosome.Random(length, genotype)
+		}
+		// Add it into population
 		p.Chromosomes = append(p.Chromosomes, tempChromosome)
 	}
 }
 
+// FindBest is a method that return the index and fitness of the best one
 func (p *population) FindBest() (bestIndex int, bestFitness float64) {
+	// Assume the first chromosome in population is the best
 	bestIndex = 0
 	bestFitness = p.Chromosomes[0].Fitness
 
+	// If the chromosome better than the best one, set it as best
 	for i := range p.Chromosomes {
 		if p.Chromosomes[i].Fitness > bestFitness {
 			bestIndex = i
