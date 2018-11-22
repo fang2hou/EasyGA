004e751227d5666b2475e0214f690a2aa856d7a0
jackH <junj2ejj@gmail.com>
2018-11-22 16:29:18 +0900

    update drawchart.go

--- _examples/noise/drawchart.go
+++ _examples/noise/drawchart.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/wcharczuk/go-chart/drawing"
 	"os"
 
 	"github.com/wcharczuk/go-chart"
@@ -11,14 +13,53 @@ func drawFitnessChart(fitnessArray []float64) {
 	xValues := make([]float64, 0)
 	for i := 0; i < iterationLength; i++ {
 		xValues = append(xValues, float64(i))
+	}
+		fitnessSeries := chart.ContinuousSeries{
+			XValues: xValues,
+			YValues: fitnessArray,
+			Style: chart.Style{
+				Show:true,
+				StrokeColor:drawing.ColorBlack,
+				StrokeDashArray:[]float64{4.0,2.0},
+
+			},
+		}
 
 		graph := chart.Chart{
-			Series: []chart.Series{
-				chart.ContinuousSeries{
-					XValues: xValues,
-					YValues: fitnessArray,
+			XAxis: chart.XAxis{
+				NameStyle: chart.StyleShow(),
+				Style:     chart.StyleShow(),
+				ValueFormatter: func(v interface{}) string {
+					if vf, isFloat := v.(float64); isFloat {
+						return fmt.Sprintf("%0.0f", vf)
+					}
+					return ""
 				},
 			},
+
+			YAxis: chart.YAxis{
+				NameStyle: chart.StyleShow(),
+				Style:     chart.StyleShow(),
+				ValueFormatter: func(v interface{}) string {
+					if vf, isFloat := v.(float64); isFloat {
+						return fmt.Sprintf("%0.0f", vf)
+					}
+					return ""
+				},
+				AxisType: chart.YAxisSecondary,
+			},
+
+			Background: chart.Style{
+				Padding: chart.Box{
+					Top:  20,
+					Left: 20,
+				},
+			},
+
+			Series: []chart.Series{
+				fitnessSeries,
+			},
+
 		}
 		filePath := "stat.png"
 		outFile, _ := os.Create(filePath)
@@ -29,4 +70,3 @@ func drawFitnessChart(fitnessArray []float64) {
 
 	}
 
-}
